68a497b30f84fb290ea8c02bd970c6ffb8e53547
Kruthik-tech <kruthik.madhams@gmail.com>
2026-09-08 10:16:44 -0700

    Update main.java

--- main.java
+++ main.java
@@ -1,6 +1,12 @@
 public class main {
     public static void main(String[] args) {
         int val1 = 3;
+        int val1 = 3;
+        val1 = val1 * 2;
+        val1 +=6;
+        val1/=2;
+        val1 -= 3;
+        System.out.println(val1);
 
         int val2 = -5;
 

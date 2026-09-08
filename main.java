2c89cfbbde8d5232384ca44701c4b9684fe67b0a
rohitsvasan <srinivasan9318@mydusd.org>
2026-09-08 10:11:20 -0700

    Update main.java

--- main.java
+++ main.java
@@ -1,5 +1,15 @@
 public class main {
     public static void main(String[] args) {
+        int val1 = 3;
 
+        int val2 = -5;
+
+        int val3 = 0;
+
+        int val4 = 1;
+
+        double val5 = 2.5;
+
+        double val6 = -3.5;
     }
 }

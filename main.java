a1456df3b35d68f2c827e0c89a8fd672f04b6093
rohitsvasan <srinivasan9318@mydusd.org>
2026-09-08 10:24:51 -0700

    Update main.java

--- main.java
+++ main.java
@@ -10,18 +10,24 @@ public class main {
         startNumber1 -= 3;
         System.out.println(startNumber1);
 
-        int val2 = -5; // negative number for testing
+        int startNumber2 = -5; // negative number for testing
         startNumber2 = startNumber2 * 2;
         startNumber2 +=6;
         startNumber2 /=2 ;
         startNumber2 -= 3;
         System.out.println(startNumber2);
-        int val3 = 0; // 0 value for testing
+        
+        int startNumber3 = 0; // 0 value for testing
 
-        int val4 = 1; // multi pos. value for testing
+        int startNumber4 = 1; // multi pos. value for testing
+        startNumber4 = startNumber4 * 2;
+        startNumber4 += 6;
+        startNumber4 /= 2;
+        startNumber4 -= 3;
+        System.out.println(startNumber4);
 
-        double val5 = 2.5; // positive double value for testing
+        double startNumber5 = 2.5; // positive double value for testing
 
-        double val6 = -3.5; // negative double value for testing
+        double startNumber6 = -3.5; // negative double value for testing
     }
 }

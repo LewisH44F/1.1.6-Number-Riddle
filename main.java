6cbf7b58a6c32ad1e72210e170ef8048c78550d3
Kruthik-tech <kruthik.madhams@gmail.com>
2026-09-08 10:23:11 -0700

    Update main.java

--- main.java
+++ main.java
@@ -11,7 +11,11 @@ public class main {
         System.out.println(startNumber1);
 
         int val2 = -5; // negative number for testing
-
+        startNumber2 = startNumber2 * 2;
+        startNumber2 +=6;
+        startNumber2 /=2 ;
+        startNumber2 -= 3;
+        System.out.println(startNumber2);
         int val3 = 0; // 0 value for testing
 
         int val4 = 1; // multi pos. value for testing

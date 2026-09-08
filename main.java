42dcebd9abf4c3850f9524699e7a99d172e863d8
Raghav G <iraghavgarg@gmail.com>
2026-09-08 10:20:50 -0700

    Add comments for clarity on test values
    Added comments to clarify the purpose of test values.

--- main.java
+++ main.java
@@ -1,20 +1,22 @@
 public class main {
     public static void main(String[] args) {
-        int startNumber1 = 3;
+
+        // Program main body for numbers
+        int startNumber1 = 3; // Positive number for testing
         startNumber1 = startNumber1 * 2;
         startNumber1 +=6;
         startNumber1/=2;
         startNumber1 -= 3;
         System.out.println(startNumber1);
 
-        int val2 = -5;
+        int val2 = -5; // negative number for testing
 
-        int val3 = 0;
+        int val3 = 0; // 0 value for testing
 
-        int val4 = 1;
+        int val4 = 1; // multi pos. value for testing
 
-        double val5 = 2.5;
+        double val5 = 2.5; // positive double value for testing
 
-        double val6 = -3.5;
+        double val6 = -3.5; // negative double value for testing
     }
 }
